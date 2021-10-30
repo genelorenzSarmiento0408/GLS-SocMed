aaa514a510d042c572d8e422b226df84c5bfb42e
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-10-30 22:58:54 +0800

    modified PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -46,9 +46,7 @@ export default function PostCard({
             </Button>
           </PopupGlobal>
 
-          {user && user.username === username && (
-            <DeleteButton postId={{ id }} />
-          )}
+          {user && user.username === username && <DeleteButton postId={id} />}
         </Card.Content>
       </Grid.Column>
     </Grid>
