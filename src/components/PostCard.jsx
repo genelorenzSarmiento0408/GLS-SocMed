d7ae3383a44b49a5dea2f0d8b969c7220a7c5e2c
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-10-30 18:13:28 +0800

    modified PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -22,7 +22,7 @@ export default function PostCard({
   },
 }) {
   const { user } = useContext(AuthContext);
-  const a = createdAt.substring(0, 10);
+  const datetostr = createdAt.substring(0, 10);
   return (
     <Grid mobile={16} tablet={8} computer={4}>
       <Grid.Column width={15} className="ui centered card">
@@ -32,7 +32,7 @@ export default function PostCard({
             size="mini"
             src="https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg"
           />
-          <PopupGlobal content={a} header={username}>
+          <PopupGlobal content={datetostr} header={username}>
             <Card.Header>{username}</Card.Header>
           </PopupGlobal>
           <Card.Meta as={Link} to={`/posts/${id}`}>
