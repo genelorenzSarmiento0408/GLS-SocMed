2df3e7e1f079fcc751a740b0e37f8bc55f1e52f4
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-10-30 22:22:20 +0800

    modified Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Grid, Transition } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
-import { FETCH_POSTS_QUERY } from "../util/graphql";
+import { FETCH_POSTS_QUERY, FETCH_USERS_QUERY } from "../util/graphql";
 import PostForm from "../components/PostForm.jsx";
 import "../App.scss";
 
@@ -37,7 +37,7 @@ export default function Home() {
                 <Grid.Column
                   key={post.id}
                   style={{ marginBottom: 20 }}
-                  width={13}
+                  width={15}
                 >
                   <PostCard post={post} />
                 </Grid.Column>
