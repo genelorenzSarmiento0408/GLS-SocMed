c4e6bddf480c67da25896f14353f3f2120663fcb
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-03 09:40:21 +0800

    modified PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -29,7 +29,7 @@ export default function PostCard({
           <PopupGlobal
             content={
               datetostr +
-              "" +
+              " " +
               (
                 <Image
                   floated="left"
