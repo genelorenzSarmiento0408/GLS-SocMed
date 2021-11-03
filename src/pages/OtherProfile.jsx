1881c569320c66b3661e910d9898aa8fd066de83
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-03 11:06:57 +0800

    modified OtherProfile.jsx

--- src/pages/OtherProfile.jsx
+++ src/pages/OtherProfile.jsx
@@ -3,6 +3,8 @@ import { gql, useQuery, useMutation } from "@apollo/client";
 
 const OtherProfile = (props, args = {}) => {
   const id = props.match.params.id;
+  console.log(postId);
+  const { user } = useContext(AuthContext);
   return <div></div>;
 };
 
