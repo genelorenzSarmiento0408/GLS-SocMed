a3d8cef289b7f7e90048fc199960d77daf85ce2a
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-04 12:10:59 +0800

    modified PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -21,7 +21,7 @@ export default function PostCard({
         <Card.Content>
           <Image
             floated="left"
-            size="mini"
+            size="small"
             src="https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg"
             avatar
           />
