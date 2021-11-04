9bc329912e4d7c81b7864918ba937f432314d397
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-04 11:46:47 +0800

    modified PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -26,13 +26,15 @@ export default function PostCard({
           />
           <PopupGlobal
             content={
-              // datetostr +
-              // " " +
-              <Image
-                floated="left"
-                size="mini"
-                src="https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg"
-              />
+              datetostr +
+              (
+                // " " +
+                <Image
+                  floated="left"
+                  size="mini"
+                  src="https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg"
+                />
+              )
             }
             header={`${username}`}
           >
