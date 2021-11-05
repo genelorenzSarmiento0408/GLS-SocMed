e9258a98c0c6598e87f45ca87d4d3972133a8b6f
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-05 21:16:52 +0800

    modified OtherProfile.jsx

--- src/pages/OtherProfile.jsx
+++ src/pages/OtherProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Card, Grid, Image, Loader, Dimmer } from "semantic-ui-react";
 
