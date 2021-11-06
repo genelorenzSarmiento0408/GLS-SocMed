34803abcfb940e087b944810d1bec699d8afbc85
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-06 13:27:27 +0800

    modified Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,12 +27,13 @@ export default function Home() {
             </Grid.Row>
           </Grid.Column>
         )}
+        { {" "}
+            <Grid.Row className="page-title" style={{ marginBottom: 20 }}>
+              <h1>Recent Posts</h1>}
         {loading && <h1>Loading posts...</h1>}
         {
           <Transition.Group>
-            {" "}
-            <Grid.Row className="page-title" style={{ marginBottom: 20 }}>
-              <h1>Recent Posts</h1>
+           
             </Grid.Row>
             {posts &&
               posts.map((post) => (
