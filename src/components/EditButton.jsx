69455a1e256cc770967c177bf82a4d9c3001d3b5
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-08 16:52:18 +0800

    modified EditButton.jsx

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useMutation, gql, useQuery } from "@apollo/client";
 import { Button, Label, Icon } from "semantic-ui-react";
 
