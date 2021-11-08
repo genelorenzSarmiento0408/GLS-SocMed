e0cffc1dabee313fb36809b08faf71b77c1f1c85
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-08 15:53:40 +0800

    modified EditButton.jsx

---
+++ src/components/EditButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { useMutation, gql } from "@apollo/client";
+import { Button, Label, Icon } from "semantic-ui-react";
+
+const EditButton = () => {
+  return <div></div>;
+};
+
+export default EditButton;
