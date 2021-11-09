dba4b9972ecf40e8daca6d25d1e9e4261e10db86
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-09 20:44:14 +0800

    modified PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -4,7 +4,7 @@ const PageNotFound = () => {
   return (
     <div>
       <h1>404 HTML ERROR</h1>
-      Oh No! Page Not Found ����
+      Oh No! Page Not Found �� (De en ef LoL)
     </div>
   );
 };
