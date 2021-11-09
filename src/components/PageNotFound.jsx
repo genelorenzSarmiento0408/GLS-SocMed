37c7ceb890104747b533907bcd6c53ca6c0b2a5d
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-09 20:43:42 +0800

    modified PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const PageNotFound = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>404 HTML ERROR</h1>
+      Oh No! Page Not Found ����
+    </div>
+  );
 };
 
 export default PageNotFound;
