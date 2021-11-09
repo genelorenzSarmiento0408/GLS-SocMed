f10a30ceb017dd98564a8a42c5c6592c63fe59bd
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-09 13:18:28 +0800

    modified PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -51,7 +51,9 @@ export default function PostCard({
             <Card.Header as={Link} to={`/posts/${id}`}>
               {title}
             </Card.Header>
-            {user && user.username === username && <EditButton postId={id} />}
+            <h1>
+              {user && user.username === username && <EditButton postId={id} />}
+            </h1>
           </PopupGlobal>
           <Card.Meta as={Link} to={`/users/${username}`}>
             {moment(createdAt).fromNow(true)} ago • by {username}{" "}
