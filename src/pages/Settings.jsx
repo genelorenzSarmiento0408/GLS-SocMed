8ef36e28f6bcaa7c2729fae304e2921513b17ad1
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:35:12 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Card } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 
@@ -7,28 +7,30 @@ export const Settings = () => {
   const { user } = useContext(AuthContext);
   let username = user.username;
   return (
-    <div className="page-title">
-      <h1>Settings</h1>
-      <Form>
-        <div className="ui action input fluid">
-          <input
-            type="text"
-            placeholder="Comment..."
-            name="Comment"
-            value={username}
-            // onChange={(event) => setComment(event.target.value)}
-            // ref={commentInputRef}
-          />
-          <button
-            type="submit"
-            className="ui button teal"
-            // disabled={comment.trim() === ""}
-            // onClick={submitComment}
-          >
-            Submit
-          </button>
-        </div>
-      </Form>
-    </div>
+    <Card fluid>
+      <div className="page-title">
+        <h1>Settings</h1>
+        <Form>
+          <div className="ui action input fluid">
+            <input
+              type="text"
+              placeholder="Comment..."
+              name="Comment"
+              value={username}
+              // onChange={(event) => setComment(event.target.value)}
+              // ref={commentInputRef}
+            />
+            <button
+              type="submit"
+              className="ui button teal"
+              // disabled={comment.trim() === ""}
+              // onClick={submitComment}
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
+      </div>
+    </Card>
   );
 };
