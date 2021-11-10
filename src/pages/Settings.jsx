f6a3b6821858218ed061a0fd143ed4f7308b5d62
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:28:48 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../context/auth";
 
 export const Settings = () => {
   const { user } = useContext(AuthContext);
+  let username = user.username;
   return (
     <div className="page-title">
       <h1>Settings</h1>
