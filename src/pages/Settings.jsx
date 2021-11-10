931e0f582ed52f8c506de8c39281f2d5bd9eb885
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:22:40 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export const Settings = () => {
   return (
     <div className="page-header">
-      <h1>Settings</h1>{" "}
+      <h1>Settings</h1>
     </div>
   );
 };
