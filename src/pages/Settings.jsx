e6b30058826f13e6fc15a51b64f6ed5b8c6e8e9e
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:37:16 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Form, Card } from "semantic-ui-react";
+import { Form, Card, Grid } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 
@@ -7,23 +7,27 @@ export const Settings = () => {
   const { user } = useContext(AuthContext);
   let username = user.username;
   return (
-    <Card fluid>
-      <div className="page-title">
-        <h1>Settings</h1>
-        <Form>
-          <div className="ui action input fluid">
-            <p>Username:</p>
-            <input
-              type="text"
-              placeholder="Username"
-              name="Comment"
-              value={username}
-              // onChange={(event) => setComment(event.target.value)}
-              // ref={commentInputRef}
-            />
+    <Grid>
+      <Grid.Row>
+        <Card fluid>
+          <div className="page-title">
+            <h1>Settings</h1>
+            <Form>
+              <div className="ui action input fluid">
+                <p>Username:</p>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  name="Comment"
+                  value={username}
+                  // onChange={(event) => setComment(event.target.value)}
+                  // ref={commentInputRef}
+                />
+              </div>
+            </Form>
           </div>
-        </Form>
-      </div>
-    </Card>
+        </Card>
+      </Grid.Row>
+    </Grid>
   );
 };
