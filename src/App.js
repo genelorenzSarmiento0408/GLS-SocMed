d325aac39902608a24ffe190943b6e61e1955c5c
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 09:59:33 +0800

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 import Navbar from "./components/NavBar";
