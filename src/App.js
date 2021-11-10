e1c43b9d2f8dde1df00f2f036a3c97d2d6295fc2
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 09:52:56 +0800

    modified App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
           <AuthRoute exact path="/login" component={Login} />
           <Route exact path="/posts/:postId" component={SinglePost} />
           <Route exact path="/users/:username" component={OtherProfile} />
-          <Route exact path="*" component={PageNotFound} />
+          <Route path="*" component={PageNotFound} />
         </Container>
       </Router>
     </AuthProvider>
