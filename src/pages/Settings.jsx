ea43b4c1b9feaab98661fc959b7eb7e117965079
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:33:20 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Form } from "semantic-ui-react";
 
 import { AuthContext } from "../context/auth";
 
@@ -8,6 +9,26 @@ export const Settings = () => {
   return (
     <div className="page-title">
       <h1>Settings</h1>
+      <Form>
+        <div className="ui action input fluid">
+          <input
+            type="text"
+            placeholder="Comment..."
+            name="Comment"
+            value={username}
+            // onChange={(event) => setComment(event.target.value)}
+            // ref={commentInputRef}
+          />
+          <button
+            type="submit"
+            className="ui button teal"
+            disabled={comment.trim() === ""}
+            // onClick={submitComment}
+          >
+            Submit
+          </button>
+        </div>
+      </Form>
     </div>
   );
 };
