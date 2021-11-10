c985ff240b8009056c31fe58a9edef5a090cab87
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:27:13 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 
 export const Settings = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="page-title">
       <h1>Settings</h1>
