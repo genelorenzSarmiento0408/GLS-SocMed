fde76e2bda63d9b7b3cc9bd86d28e274de8a85d2
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-10 21:24:25 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const Settings = () => {
   return (
-    <div className="page-header">
+    <div className="page-title">
       <h1>Settings</h1>
     </div>
   );
