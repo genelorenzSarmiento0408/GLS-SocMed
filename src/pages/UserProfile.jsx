5a8a067ace0eca7dc363339759ef6d83b2d282e2
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-13 20:18:29 +0800

    modified UserProfile.jsx

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -7,63 +7,63 @@ import { AuthContext } from "../context/auth";
 
 const UserProfile = (args = {}) => {
   const { user } = useContext(AuthContext);
-  if (user) {
-    const username = user.username;
+  const username = user.username;
 
-    let userMarkup;
-    const { loading, data: { getUser } = args } = useQuery(FETCH_USER_QUERY, {
-      variables: {
-        username,
-      },
-    });
+  let userMarkup;
+  const { loading, data: { getUser } = args } = useQuery(FETCH_USER_QUERY, {
+    variables: {
+      username,
+    },
+  });
 
-    if (!getUser) {
-      userMarkup = loading && (
-        <Dimmer active>
-          <Loader content="Loading user..." />
-        </Dimmer>
-      );
-    } else {
-      let isbionull = false;
-      const { Bio, createdAt, username } = getUser;
-      const datetostr = createdAt.substring(0, 10);
-      if (Bio === null) {
-        isbionull = true;
-      }
-
-      userMarkup = (
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={2}>
-              <Image
-                src="https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg"
-                size="small"
-                float="right"
-              />
-            </Grid.Column>
-            <Grid.Column width={10}>
-              <Card fluid>
-                <Card.Content>
-                  <Card.Header>{username}</Card.Header>
-                  <Card.Meta>{`user created at: ${datetostr}`}</Card.Meta>
-                  <Card.Description>
-                    {isbionull ? "No Bio added" : Bio}
-                  </Card.Description>
-                </Card.Content>
-                <hr />
-                <Card.Content extra>
-                  <Button as={Link} to={"/settings"} color="facebook">
-                    Settings
-                  </Button>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      );
+  if (!getUser) {
+    userMarkup = loading && (
+      <Dimmer active>
+        <Loader content="Loading user..." />
+      </Dimmer>
+    );
+  } else {
+    let isbionull = false;
+    const { Bio, createdAt, username } = getUser;
+    const datetostr = createdAt.substring(0, 10);
+    if (Bio === null) {
+      isbionull = true;
     }
+
+    userMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              src="https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg"
+              size="small"
+              float="right"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{`user created at: ${datetostr}`}</Card.Meta>
+                <Card.Description>
+                  {isbionull ? "No Bio added" : Bio}
+                </Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content extra>
+                <Button as={Link} to={"/settings"} color="facebook">
+                  Settings
+                </Button>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
   }
+
   return userMarkup;
+  if (!user) return <></>;
 };
 
 const FETCH_USER_QUERY = gql`
