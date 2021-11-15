caaee7f3043ca6f58d89d2b91e040dcbd9623410
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-15 10:15:25 +0800

    modified UserRoute.js

--- src/util/UserRoute.js
+++ src/util/UserRoute.js
@@ -4,7 +4,7 @@ import { Route } from "react-router-dom";
 import { AuthContext } from "../context/auth";
 import PageNotFound from "../pages/PageNotFound";
 
-function userRoute({ component: Component, ...rest }) {
+export default function UserRoute({ component: Component, ...rest }) {
   const { user } = useContext(AuthContext);
 
   return (
@@ -16,4 +16,3 @@ function userRoute({ component: Component, ...rest }) {
     />
   );
 }
-export default userRoute;
