584e2dd817d3c3031251df7f3dc932f31b8ff812
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-16 19:51:31 +0800

    modified EditOrAddBio.jsx

--- src/components/EditOrAddBio.jsx
+++ src/components/EditOrAddBio.jsx
@@ -5,7 +5,18 @@ import { AuthContext } from "../context/auth";
 
 const EditOrAddBio = () => {
   const { user } = useContext(AuthContext);
-
+  const Bio = user.Bio;
+  const editoraddbio = Bio != null ? "" : "";
+  const editBio = (
+    <>
+      <p>Edit Bio</p>
+    </>
+  );
+  const addBio = (
+    <>
+      <button>Add Bio</button>
+    </>
+  );
   return <div></div>;
 };
 
