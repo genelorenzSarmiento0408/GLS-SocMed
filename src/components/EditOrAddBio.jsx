5b9b4b66537fe0ed240d8dc2d05e4ed1420bde89
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-16 19:43:27 +0800

    modified EditOrAddBio.jsx

--- src/components/EditOrAddBio.jsx
+++ src/components/EditOrAddBio.jsx
@@ -1,7 +1,13 @@
+import gql from "graphql-tag";
 import React from "react";
 
 const EditOrAddBio = () => {
   return <div></div>;
 };
+
+const EDITORADDBIO = gql`
+  mutation EDITORADDBIO($username: String!, $newBio: String!)
+`;
+gql;
 
 export default EditOrAddBio;
