04c55b1871968ede49d7255529b07883f7a0ab9b
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-16 19:52:46 +0800

    modified EditOrAddBio.jsx

--- src/components/EditOrAddBio.jsx
+++ src/components/EditOrAddBio.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from "../context/auth";
 const EditOrAddBio = () => {
   const { user } = useContext(AuthContext);
   const Bio = user.Bio;
-  const editoraddbio = Bio != null ? "" : "";
+  const determinebio = Bio != null ? editBio : addBio;
   const editBio = (
     <>
       <p>Edit Bio</p>
@@ -17,7 +17,7 @@ const EditOrAddBio = () => {
       <button>Add Bio</button>
     </>
   );
-  return <div></div>;
+  return determinebio;
 };
 
 const EDITORADDBIO = gql`
