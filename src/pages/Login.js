53e29744e55117e8947c2e5c0222b4332db9087f
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-17 14:16:07 +0800

    modified Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,6 @@ export default function Login(props) {
 
   const [loginUser, { loading }] = useMutation(LOGIN_USER, {
     update(_, { data: { login: userData } }) {
-      console.log(userData);
       context.login(userData);
       props.history.push("/");
     },
