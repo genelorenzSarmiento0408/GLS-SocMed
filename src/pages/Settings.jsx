7a554f05113d384b6f7cdd717dcd27acf86e7581
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-17 15:48:43 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -82,15 +82,14 @@ export const Settings = () => {
                   Change Password
                 </button>
                 {Object.keys(errors).length > 0 && (
-                      <div className="ui error message">
-                        <ul className="list">
-                          {Object.values(errors).map((value) => (
-                            <li key={value}>{value}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    )
-                  :}
+                  <div className="ui error message">
+                    <ul className="list">
+                      {Object.values(errors).map((value) => (
+                        <li key={value}>{value}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
               </div>
               <Divider />
               <EditOrAddBio />
