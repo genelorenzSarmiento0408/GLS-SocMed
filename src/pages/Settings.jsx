732fc204e718eb7dcb4d29c71855dd772c84f128
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-17 14:17:23 +0800

    modified Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -75,6 +75,15 @@ export const Settings = () => {
                 >
                   Change Password
                 </button>
+                {Object.keys(errors).length > 0 && (
+                  <div className="ui error message">
+                    <ul className="list">
+                      {Object.values(errors).map((value) => (
+                        <li key={value}>{value}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
               </div>
               <Divider />
               <EditOrAddBio />
