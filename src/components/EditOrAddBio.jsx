75dc46664e5f1bb089f47700d8d0d65130a663a0
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-18 20:24:20 +0800

    modified EditOrAddBio.jsx

--- src/components/EditOrAddBio.jsx
+++ src/components/EditOrAddBio.jsx
@@ -7,7 +7,7 @@ import { AuthContext } from "../context/auth";
 const EditOrAddBio = (args = {}) => {
   const { user } = useContext(AuthContext);
   let username = user.username;
-  const Bio = user.Bio;
+
   const { data: { getUser } = args } = useQuery(FETCH_USER_QUERY, {
     variables: {
       username,
