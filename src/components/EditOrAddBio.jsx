f4e9aaf134dc1bd32829f53f03592e964554c644
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-18 20:17:11 +0800

    modified EditOrAddBio.jsx

--- src/components/EditOrAddBio.jsx
+++ src/components/EditOrAddBio.jsx
@@ -14,7 +14,7 @@ const EditOrAddBio = (args = {}) => {
       Bio,
     },
   });
-  console.info(getUser.username);
+  console.info(getUser.Bio);
   const editBio = (
     <>
       <h2>Edit Bio</h2>
