1509dcce0cad7f120136f0b6d21cd77b0606d575
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-22 11:54:34 +0800

    modified App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <Container>
+        <Container textAlign="center">
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
