3206abbf3b2e34401797e094e6dee508f140d6dd
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2021-11-22 11:56:17 +0800

    modified App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <Grid fluid>
+        <Grid>
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
